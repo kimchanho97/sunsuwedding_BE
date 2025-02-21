ed95ed3d004013c8c127ec3cc7764080f30b1bd4
kimchanho97 <nh0903@pusan.ac.kr>
2025-02-21 19:58:12 +0900

    initial commit

---
+++ src/main/java/study/sunsuwedding/SunsuweddingApplication.java
@@ -0,0 +1,13 @@
+package study.sunsuwedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunsuweddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunsuweddingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/sunsuwedding/SunsuweddingApplicationTests.java
@@ -0,0 +1,13 @@
+package study.sunsuwedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunsuweddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
