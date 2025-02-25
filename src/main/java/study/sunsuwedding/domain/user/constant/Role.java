1c864dcfabe9eeb563f74765fd84e40e6f56d6a6
kimchanho97 <nh0903@pusan.ac.kr>
2025-02-26 00:06:28 +0900

    feat: Role 생성 메서드 구현

--- src/main/java/study/sunsuwedding/domain/user/constant/Role.java
+++ src/main/java/study/sunsuwedding/domain/user/constant/Role.java
@@ -2,6 +2,9 @@ package study.sunsuwedding.domain.user.constant;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import study.sunsuwedding.domain.user.exception.UserException;
+
+import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor
@@ -10,4 +13,11 @@ public enum Role {
     PLANNER("planner");
 
     private final String roleName;
+
+    public static Role fromString(String role) {
+        return Arrays.stream(values())
+                .filter(value -> value.roleName.equalsIgnoreCase(role)) // 대소문자 구분 없이 변환
+                .findFirst()
+                .orElseThrow(UserException::invalidRole);
+    }
 }
