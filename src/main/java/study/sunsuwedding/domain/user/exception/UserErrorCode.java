232e22ae7dfb8eb4c88cbe818e842e9bc527d62d
kimchanho97 <nh0903@pusan.ac.kr>
2025-02-26 00:05:42 +0900

    feat: UserException 추가

--- src/main/java/study/sunsuwedding/domain/user/exception/UserErrorCode.java
+++ src/main/java/study/sunsuwedding/domain/user/exception/UserErrorCode.java
@@ -9,8 +9,15 @@ import study.sunsuwedding.common.exception.ErrorCode;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, 2000, "서비스를 탈퇴했거나 가입하지 않은 유저의 요청입니다."),
-    ;
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, 2000, "해당 사용자를 찾을 수 없습니다. 탈퇴했거나 존재하지 않는 계정입니다."),
+    PLANNER_NOT_FOUND(HttpStatus.NOT_FOUND, 2001, "해당 플래너를 찾을 수 없습니다. 탈퇴했거나 존재하지 않는 계정입니다."),
+    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, 2002, "해당 이메일로 가입된 계정이 이미 존재합니다."),
+    EMAIL_NOT_FOUND(HttpStatus.BAD_REQUEST, 2003, "이메일을 찾을 수 없습니다."),
+    INVALID_ROLE(HttpStatus.BAD_REQUEST, 2004, "역할(Role)은 'PLANNER' 또는 'COUPLE'만 가능합니다."),
+    INCORRECT_PASSWORD(HttpStatus.BAD_REQUEST, 2005, "비밀번호가 일치하지 않습니다."),
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, 2006, "비밀번호와 비밀번호 확인 값이 일치해야 합니다."),
+    ALREADY_PREMIUM_USER(HttpStatus.BAD_REQUEST, 2007, "이미 프리미엄 회원입니다."),
+    DELETED_USER(HttpStatus.BAD_REQUEST, 2008, "이미 탈퇴한 회원입니다. 계정을 복구하시겠습니까?");
 
     private final HttpStatus httpStatus;
     private final int code;
--- src/main/java/study/sunsuwedding/domain/user/exception/UserException.java
+++ src/main/java/study/sunsuwedding/domain/user/exception/UserException.java
@@ -11,4 +11,37 @@ public class UserException extends BusinessException {
     public static UserException userNotFound() {
         return new UserException(UserErrorCode.USER_NOT_FOUND);
     }
+
+    public static UserException plannerNotFound() {
+        return new UserException(UserErrorCode.PLANNER_NOT_FOUND);
+    }
+
+    public static UserException duplicateEmail() {
+        return new UserException(UserErrorCode.DUPLICATE_EMAIL);
+    }
+
+    public static UserException emailNotFound() {
+        return new UserException(UserErrorCode.EMAIL_NOT_FOUND);
+    }
+
+    public static UserException invalidRole() {
+        return new UserException(UserErrorCode.INVALID_ROLE);
+    }
+
+    public static UserException incorrectPassword() {
+        return new UserException(UserErrorCode.INCORRECT_PASSWORD);
+    }
+
+    public static UserException passwordMismatch() {
+        return new UserException(UserErrorCode.PASSWORD_MISMATCH);
+    }
+
+    public static UserException alreadyPremiumUser() {
+        return new UserException(UserErrorCode.ALREADY_PREMIUM_USER);
+    }
+
+    public static UserException deletedUser() {
+        return new UserException(UserErrorCode.DELETED_USER);
+    }
 }
+
