2f6e03f7e1cde06c2c3a42b55a9c5e9d7d51037b
kimchanho97 <nh0903@pusan.ac.kr>
2025-02-26 01:25:05 +0900

    feat: TypeMismatch, Validation 예외 핸들러 구현

--- src/main/java/study/sunsuwedding/common/exception/GlobalExceptionHandler.java
+++ src/main/java/study/sunsuwedding/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package study.sunsuwedding.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import study.sunsuwedding.common.response.ErrorResponse;
 
 @Slf4j
@@ -17,5 +21,42 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(e.getHttpStatus())
                 .body(new ErrorResponse(e));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getDefaultMessage())
+                .findFirst()
+                .orElse("잘못된 요청입니다.");
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(400, errorMessage));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(CommonErrorCode.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(CommonErrorCode.SYSTEM_ERROR));
+    }
+
+
+    // DB 예외는 DataAccessException 하나로 통합 처리
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<ErrorResponse> handleDatabaseException(DataAccessException e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(CommonErrorCode.SYSTEM_ERROR));
     }
 }
--- src/main/java/study/sunsuwedding/common/response/ErrorResponse.java
+++ src/main/java/study/sunsuwedding/common/response/ErrorResponse.java
@@ -2,6 +2,7 @@ package study.sunsuwedding.common.response;
 
 import lombok.Getter;
 import study.sunsuwedding.common.exception.BaseException;
+import study.sunsuwedding.common.exception.ErrorCode;
 
 @Getter
 public class ErrorResponse {
@@ -12,5 +13,15 @@ public class ErrorResponse {
     public ErrorResponse(BaseException e) {
         this.code = e.getCode();
         this.message = e.getMessage();
+    }
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
+
+    public ErrorResponse(int code, String message) {
+        this.code = code;
+        this.message = message;
     }
 }
