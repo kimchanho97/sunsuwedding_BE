f201cbd582ee5564018847f58996115d0949274b
kimchanho97 <nh0903@pusan.ac.kr>
2025-03-05 17:55:02 +0900

    feat: UserInfoResponse DTO 정의

---
+++ src/main/java/study/sunsuwedding/domain/user/dto/res/UserInfoResponse.java
@@ -0,0 +1,29 @@
+package study.sunsuwedding.domain.user.dto.res;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import study.sunsuwedding.common.util.DateFormatter;
+import study.sunsuwedding.domain.payment.entity.Payment;
+import study.sunsuwedding.domain.user.entity.User;
+
+@Getter
+@RequiredArgsConstructor
+public class UserInfoResponse {
+
+    private final Long userId;
+    private final String username;
+    private final String email;
+    private final String role;
+    private final String grade;
+    private final String payedAt;
+
+    public static UserInfoResponse fromEntity(User user, Payment payment) {
+        return new UserInfoResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getDtype(),
+                user.getGrade().getGradeName(),
+                payment != null ? DateFormatter.formatDateInKorean(payment.getPayedAt()) : "");
+    }
+}
