390fa8688735e4752d3a744047b0baf1b689367a
kimchanho97 <nh0903@pusan.ac.kr>
2025-03-07 00:53:41 +0900

    feat: Portfolio 엔티티 정의

---
+++ src/main/java/study/sunsuwedding/domain/portfolio/entity/Portfolio.java
@@ -0,0 +1,59 @@
+package study.sunsuwedding.domain.portfolio.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import study.sunsuwedding.domain.user.entity.Planner;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Table(name = "portfolio")
+@Getter
+@SQLDelete(sql = "UPDATE portfolio SET is_deleted = true, deleted_at = NOW() WHERE portfolio_id = ?")
+@SQLRestriction("is_deleted = false")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "portfolio_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "planner_id")
+    private Planner planner;
+
+    @Column(nullable = false)
+    private String plannerName;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false, length = 1000)
+    private String description;
+
+    @Column(nullable = false, length = 1000)
+    private String career;
+
+    @Column(nullable = false, length = 1000)
+    private String partnerCompany;
+
+    private Long totalPrice;
+    private Long contractCount;
+    private Long avgPrice;
+    private Long minPrice;
+    private Long maxPrice;
+    private Double avgStars;
+    private Boolean isDeleted;
+    private LocalDateTime deletedAt;
+
+}
