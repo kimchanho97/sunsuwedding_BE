99d98ee4f835d828dbf0a8c0d1b095eba6d00e67
kimchanho97 <nh0903@pusan.ac.kr>
2025-03-07 00:54:00 +0900

    feat: PortfolioImage 엔티티 정의

---
+++ src/main/java/study/sunsuwedding/domain/portfolio/entity/PortfolioImage.java
@@ -0,0 +1,32 @@
+package study.sunsuwedding.domain.portfolio.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Table(name = "portfolio_image")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PortfolioImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "portfolio_image_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "portfolio_id")
+    private Portfolio portfolio;
+
+    private String fileName; // S3에 저장된 파일명(Key)
+    private String imageUrl; // S3에 저장된 이미지 URL
+
+    @Column(nullable = false)
+    private Boolean isThumbnail;
+
+
+}
