d910166a6d69c257f44773c381ae3c78e5cf78c7
kimchanho97 <nh0903@pusan.ac.kr>
2025-04-09 21:18:57 +0900

    feat: 찜하기 constraint 추가

--- src/main/java/study/sunsuwedding/domain/favorite/entity/Favorite.java
+++ src/main/java/study/sunsuwedding/domain/favorite/entity/Favorite.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 import study.sunsuwedding.common.entity.BaseTimeEntity;
 import study.sunsuwedding.domain.portfolio.entity.Portfolio;
 import study.sunsuwedding.domain.user.entity.User;
@@ -11,8 +12,10 @@ import study.sunsuwedding.domain.user.entity.User;
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
-@Table(name = "favorite")
+@Table(name = "favorite",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "portfolio_id"}))
 @Getter
+@SQLRestriction("is_deleted = false")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Favorite extends BaseTimeEntity {
 
@@ -29,8 +32,7 @@ public class Favorite extends BaseTimeEntity {
     @JoinColumn(name = "portfolio_id")
     private Portfolio portfolio;
 
-    public Favorite(User user, Portfolio portfolio) {
-        this.user = user;
-        this.portfolio = portfolio;
-    }
+    @Column(nullable = false)
+    private boolean isDeleted;
+
 }
