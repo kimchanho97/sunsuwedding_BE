9cc24bf6dc8e4fc74a510d782e85ef958d181c91
kimchanho97 <nh0903@pusan.ac.kr>
2025-04-17 02:59:06 +0900

    chore: Portfolio 상수 추가

---
+++ src/main/java/study/sunsuwedding/domain/portfolio/constant/PortfoliioConst.java
@@ -0,0 +1,9 @@
+package study.sunsuwedding.domain.portfolio.constant;
+
+public class PortfoliioConst {
+
+    public final static int MAX_PORTFOLIO_IMAGES_COUNT = 5;
+
+    private PortfoliioConst() {
+    }
+}
