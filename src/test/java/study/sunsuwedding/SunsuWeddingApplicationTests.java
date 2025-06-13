26997466b424fe7ee38ad2be5da126c77b014e6e
kimchanho97 <nh0903@pusan.ac.kr>
2025-06-13 15:15:20 +0900

    refactor: Github Actions에서 테스트 실행

--- src/test/java/study/sunsuwedding/SunsuWeddingApplicationTests.java
+++ src/test/java/study/sunsuwedding/SunsuWeddingApplicationTests.java
@@ -1,7 +1,10 @@
 package study.sunsuwedding;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class SunsuWeddingApplicationTests {
@@ -10,4 +13,9 @@ class SunsuWeddingApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    @DisplayName("CI/CD 파이프라인 테스트 - 의도적 실패")
+    void cicdTest() {
+        fail("CI/CD 테스트용 의도적 실패");
+    }
 }
